c685a80b7d7029db4a9877bd8e9322e9f5bc455b
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-04-05 10:28:54 +1200

    Trialing of add

--- 02_add_v1.py
+++ 02_add_v1.py
@@ -18,7 +18,10 @@ combos = {"VALUE":
              "Smoothie": 2.00}
           }
 
+new_combo = {}
+
 # Get items in combos
+
 text = "Enter the following combo items"  # Prompt for user to type
 title = "Enter combo"  # Title shown
 input_list = ["Combo name", "Burger", "Side", "Drink"]  # Fields to fill in
@@ -26,18 +29,30 @@ input_list = ["Combo name", "Burger", "Side", "Drink"]  # Fields to fill in
 items = easygui.multenterbox(text, title, input_list)
 
 # Get prices of combos
+
 price_text = "Enter the prices of your items"  # Prompt user for prices
 price_title = "Enter prices"  # Title shown
 # Prices to fill
-input_price = [f"{items[1].upper()}", f"{items[2]}", f"{items[3]}"]
+input_price = [f"{items[1]}", f"{items[2]}", f"{items[3]}"]
 # Prices added to a list
 prices = easygui.multenterbox(price_text, price_title, input_price)
 
-# Add the user's combo to the existing combos
+# Add the user's combo to a new dictionary
 
+new_combo[items[0]] = {}  # Adds key of combo name and empty dictionary
+new_combo[items[0]][items[1]] = prices[0]  # Adds burger and price
+new_combo[items[0]][items[2]] = prices[1]  # Adds side and price
+new_combo[items[0]][items[3]] = prices[2]  # Adds drink and price
 
-easygui.msgbox(f"Is the following combo correct?\n\n"
-               f"{items[0].upper()}\n"
-               f"{items[1]}: ${prices[0]}\n"
-               f"{items[2]}: ${prices[1]}\n"
-               f"{items[3]}: ${prices[2]}\n")
+# Print the combo and check with user that it is correct
+combo = ""
+for combo_name, combo_info in new_combo.items():
+    name = f"{combo_name}"
+
+    for food in combo_info:
+        combo += f"{food}: {combo_info[food]}\n"
+
+easygui.buttonbox(f"Is the following combo correct?\n\n"
+                  f"{name}\n\n"
+                  f"{combo}",
+                  choices=["Yes", "No"])
