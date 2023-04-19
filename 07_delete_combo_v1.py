8e05e187d7d527fc1b49ebda8e1867da5c08bda7
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-04-19 18:31:12 +1200

    Delete combo

---
+++ 07_delete_combo_v1.py
@@ -0,0 +1,59 @@
+"""Version 1 of component 7 to delete combo"""
+
+import easygui
+
+# Stores burger combos in a nested dictionary
+combos = {"VALUE":
+            {"Beef Burger": "$5.69",
+             "Fries": "$1.00",
+             "Fizzy Drink": "$1.00"},
+          "CHEEZY":
+            {"Cheeseburger": "$6.69",
+             "Fries": "$1.00",
+             "Fizzy Drink": "$1.00"},
+          "SUPER":
+            {"Cheeseburger": "$6.69",
+             "Large Fries": "$2.00",
+             "Smoothie": "$2.00"}
+          }
+
+combo = ""
+for combo_name, combo_info in combos.items():
+    name = f"{combo_name}"
+
+    for key in combo_info:
+        combo += f"{key}: {combo_info[key]}\n"
+
+
+menu = ""
+
+# Loop to print full menu
+for combo_name, combo_info in combos.items():
+
+    # Print the combo name
+    menu += f"\n{combo_name}\n"
+
+    # Print the combo items and price
+    for key, value in combo_info.items():
+        menu += f"{key}: {value} \n"
+
+# Output the full menu and ask the user what combo to delete
+choice = easygui.enterbox(f"/ / / Below is the full menu of combos/ / /\n\n"
+                          f"{menu}\n\n"
+                          "Which combo would you like to delete?").upper()
+
+# Show error message if the combo is not in the menu
+
+while choice not in combos:
+    easygui.msgbox(f"Sorry, {choice} is not in the menu", "ERROR")
+
+    # Output the full menu and ask the user what combo to delete
+    choice = easygui.enterbox(f"/ / / Below is the full menu of combos/ / /\n\n"
+                              f"{menu}\n\n"
+                              "Which combo would you like to delete?",
+                              "ENTER COMBO NAME").upper()
+
+# Delete the combo
+del[combos[choice]]
+easygui.msgbox(f"{choice} has been deleted", "DELETED")
+
